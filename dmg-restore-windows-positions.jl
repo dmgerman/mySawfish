505df399071fefa7e1ddab43cfb0dada1c713963
Daniel M German <dmg@uvic.ca>
2016-01-16 12:34:17 -0800

    updated header

--- dmg-restore-windows-positions.jl
+++ dmg-restore-windows-positions.jl
@@ -1,4 +1,4 @@
-;; dmg-restore-windows-positions.jl 0.1 -- store state across window instances
+;; dmg-restore-windows-positions.jl 0.1 -- restore windows to position when changing back to a given screen size
 
 ;; Copyright (C) 2016 Daniel M German <dmg@turingmachine.org>
 
